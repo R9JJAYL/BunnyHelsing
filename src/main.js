3d550e854f249ac65569628753dfa52ff032b491
Jamie Lyons <141517744+R9JJAYL@users.noreply.github.com>
2026-02-06 22:25:25 +0000

    Hide tutorial when level completes to prevent popup overlap

--- src/main.js
+++ src/main.js
@@ -2983,6 +2983,13 @@ class GameScene extends Phaser.Scene {
       this.bullet = null;
     }
 
+    // Hide tutorial if active
+    if (this.tutorialActive) {
+      this.tutorialActive = false;
+      if (this.tutorialContainer) this.tutorialContainer.setVisible(false);
+      if (this.tutorialArrow) this.tutorialArrow.setVisible(false);
+    }
+
     this.levelEnded = true;
 
     const overlay = this.add.rectangle(600, 325, 1200, 650, 0x000000, 0.85);
